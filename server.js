3ca09467f5ceb98ed2176ea68c2c04759b13603f
vivek23198 <guptavivek23198@gmail.com>
2020-04-25 13:26:21 +0530

    Second commit

--- server.js
+++ server.js
@@ -84,6 +84,6 @@ io.on('connection', socket => {
 
 
 
-const PORT = 3000 || process.env.PORT;
+const port =  process.env.PORT || 3000;
 
-server.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
+server.listen(port, ()=> console.log(`Server running on port ${port}`));
